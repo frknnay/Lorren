6c920cdf5af7e0da249df1c79a8dade0447320e9
Furkan <furkan-ay@outlook.com>
2017-06-23 17:07:46 +0200

    Created authors app

--- Lorren/Lorren/urls.py
+++ Lorren/Lorren/urls.py
@@ -4,7 +4,11 @@ from django.conf.urls import url, include
 from books import views as books_views
 from books import urls as books_urls
 
+from authors import urls as authors_urls
+
 urlpatterns = [
     url(r'^$', books_views.home_page, name='home'),
     url(r'^books/', include(books_urls)),
+    url(r'^authors/', include(authors_urls)),
+
 ]
